a9b53a9b563ec9b0ab886a196b370350e209429d
Jordan Staniscia <jordan.staniscia@gmail.com>
2015-02-23 01:26:53 -0800

    added reset, you will scroll to bottom when new messages arrive, added clears

--- js/jquery.talk.js
+++ js/jquery.talk.js
@@ -1,45 +1,88 @@
-function create_message (questions, i) {
+var questions = [
+						["Hi.", "I'm the computer.", "What's your name?"],
+						["Awesome name.", "What city are you in right now?"],
+						["Yes?"],
+						["What temperature do you think it is outside?"],
+						["It was nice meeting you."]
+					]
+var currentQuestion = 0,
+	lastQuestion = questions.length - 1;
+
+function story_controller (questions) {
+	current = 0;
+	create_message(questions[current]);
+}
+
+// Recursive function that goes through the set of messages it is given
+function create_message (messagesArray, i) {
 
 	i = typeof i !== 'undefined' ? i : 0;
 
 	var htmlWrapperBeginning = "<div class=\"line\"><div class=\"message message-left animated fadeInUp\">",
 		htmlWrapperEnding = "</div></div>";
 
-	var delay = (i > 0) ? calculateDelay(questions[i - 1]) : 100;
-	console.log(delay);
+	var delay = (i > 0) ? calculateDelay(messagesArray[i - 1]) : 100;
 
 	setTimeout(function(){
-		if (i >= questions.length) {
+		if (i >= messagesArray.length) {
 			create_answer_field(delay);
 			return 1;
 		}
 		else {
 
-			$('#container').append(htmlWrapperBeginning + questions[i] + htmlWrapperEnding);
+			$('#container').append(htmlWrapperBeginning + messagesArray[i] + htmlWrapperEnding);
+			smoothScrollBottom();
 			i++;
-			new create_message (questions, i)
+			create_message (messagesArray, i)
 		}
 	}, delay);
 }
 
+// Creates an answer input bubble
 function create_answer_field () {
 	var htmlAnswerField = "<div class=\"line\"><input type=\"text\" name=\"answer\" id=\"answer\" class=\"message message-right animated fadeInUp\" value=\"\" placeholder=\"Write a response…\"></div>";
 
+	if (currentQuestion >= lastQuestion) {
+		return 1;
+	}
+
 	$('#container').append(htmlAnswerField);
+	smoothScrollBottom();
+
+	$('#answer').keyup(function(event){
+	    if(event.keyCode == 13){
+	    	var answer = $('#answer').val();
+	    	$('#answer').remove();
+	        create_answer_message(answer);
+	    }
+	});
+
 	$('#answer').focus();
 }
 
+function create_answer_message (message) {
+	var htmlWrapperBeginning = "<div class=\"line\"><div class=\"message message-right animated pulse\">",
+		htmlWrapperEnding = "</div></div><div class=\"clear\"></div>";
+
+	$('#container').append(htmlWrapperBeginning + message + htmlWrapperEnding);
+
+	currentQuestion++;
+	create_message(questions[currentQuestion]);
+}
+
+// Calculates the delay based on whatever string you give it
 function calculateDelay (string) {
-	// 275 words per minute in milliseconds
-	return (string.split(" ").length * 218) + 400;
+	// 275 words per minute in milliseconds plus a constant
+	var delayPerWord = 218;
+	var delay = string.split(" ").length * delayPerWord;
+	delay = (delay < delayPerWord * 3) ? delay + 400 : delay;
+	return delay;
+}
+
+function smoothScrollBottom () {
+	$('html,body').animate({ scrollTop: $(document).height() }, 100);
 }
 
 $(document).ready(function() {
-	var questions = [
-						["Hi.", "I'm the computer.", "What's your name?"],
-						["Awesome name.", "What city are you in right now?"]
-					]
-	var question = 0;
-
-	new create_message(questions[question]);
+	new story_controller(questions);
 });
