4ddd816b4fec1bc3291a7fee6d663d1fb8735c39
Jordan Staniscia <jordanstaniscia@BLK-Pro-Pro-Pro.local>
2026-01-05 20:45:47 -0800

    New site! No more chat on the front page.

--- js/menu.js
+++ js/menu.js
@@ -1,26 +1,26 @@
 var menuString = `
 <div id="menu">
   <ul>
-    <li id="chat">
-      <a href="/index.html">
+    <li>
+      <a href="#about">
         <svg xmlns="http://www.w3.org/2000/svg" data-name="circle" viewBox="0 0 24 24"><title>circle</title><g data-name="Icon/Circle"><circle class="circle" cx="12" cy="12" r="11"/></g></svg>
-        Home
-      </a>
-    </li>
-    <li id="work">
-      <a href="/work.html">
-        <svg xmlns="http://www.w3.org/2000/svg" data-name="triangle" viewBox="0 0 24 24"></defs><title>triangle</title><g data-name="Icon/Triangle"><path class="triangle" fill-rule="evenodd" clip-rule="evenodd" d="M10.2995 3.74701C11.0814 2.48396 12.9187 2.48396 13.7006 3.74701L23.1103 18.9473C23.9351 20.2797 22.9767 22 21.4097 22H2.59033C1.02331 22 0.0649925 20.2797 0.8898 18.9473L10.2995 3.74701Z"></g></svg>
-        Work
-      </a>
-    </li>
-    <li id="about" class="active">
-      <a href="/about.html">
-        <svg xmlns="http://www.w3.org/2000/svg" data-name="square" viewBox="0 0 24 24"><title>square</title><g data-name="Icon/Square"><path class="square" d="M2 4C2 2.89543 2.89543 2 4 2H20C21.1046 2 22 2.89543 22 4V20C22 21.1046 21.1046 22 20 22H4C2.89543 22 2 21.1046 2 20V4Z"></g></svg>
         About
       </a>
     </li>
-    <li id="contact">
-      <a href="/contact.html">
+    <li class="active">
+      <a href="#work">
+        <svg xmlns="http://www.w3.org/2000/svg" data-name="square" viewBox="0 0 24 24"><title>square</title><g data-name="Icon/Square"><path class="square" d="M2 4C2 2.89543 2.89543 2 4 2H20C21.1046 2 22 2.89543 22 4V20C22 21.1046 21.1046 22 20 22H4C2.89543 22 2 21.1046 2 20V4Z"></g></svg>
+        Work
+      </a>
+    </li>
+    <li>
+      <a href="#writing">
+        <svg xmlns="http://www.w3.org/2000/svg" data-name="triangle" viewBox="0 0 24 24"></defs><title>triangle</title><g data-name="Icon/Triangle"><path class="triangle" fill-rule="evenodd" clip-rule="evenodd" d="M10.2995 3.74701C11.0814 2.48396 12.9187 2.48396 13.7006 3.74701L23.1103 18.9473C23.9351 20.2797 22.9767 22 21.4097 22H2.59033C1.02331 22 0.0649925 20.2797 0.8898 18.9473L10.2995 3.74701Z"></g></svg>
+        Writing
+      </a>
+    </li>
+    <li>
+      <a href="#contact">
         <svg xmlns="http://www.w3.org/2000/svg" data-name="half-circle" viewBox="0 0 24 24"></defs><title>half circle</title><g data-name="Icon/halfcircle"><path class="half-circle" d="M20.144 1.31703C24.4366 6.81523 24.8949 14.6616 19.8891 19.6673C14.8834 24.6731 7.03706 24.2148 1.53885 19.9222C0.668207 19.2424 0.674428 17.9646 1.45548 17.1835L17.4053 1.23365C18.1864 0.452604 19.4642 0.446381 20.144 1.31703Z"></g></svg>
         Contact
       </a>
